35ab9d350472dcbff0a7ee1621fe590dec6e642e
MURAKI Taichi <muraki.taichi@gmail.com>
2022-02-28 18:28:57 +0900

    f

--- process_hand_image.py
+++ process_hand_image.py
@@ -4,11 +4,10 @@ import mediapipe as mp
 import mediapipe.python.solutions as solutions
 
 mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
+mp_drawing_styles = solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For static images:
-IMAGE_FILES = []
+
 
 
 def main(filename):
@@ -30,28 +29,12 @@ def main(filename):
         image_height, image_width, _ = image.shape
         annotated_image = image.copy()
         for hand_landmarks in results.multi_hand_landmarks:
-            print("hand_landmarks:", hand_landmarks)
+            #print("hand_landmarks:", hand_landmarks)
             print(
                 f"Index finger tip coordinates: (",
                 f"{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, "
                 f"{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})",
             )
-            mp_drawing.draw_landmarks(
-                annotated_image,
-                hand_landmarks,
-                mp_hands.HAND_CONNECTIONS,
-                mp_drawing_styles.get_default_hand_landmarks_style(),
-                mp_drawing_styles.get_default_hand_connections_style(),
-            )
-        cv2.imwrite(
-            "annotated_hand_image.png", cv2.flip(annotated_image, 1)
-        )
-        # # Draw hand world landmarks.
-        # if not results.multi_hand_world_landmarks:
-        #     raise 
+        print(len(hand_landmarks.landmark))
 
-        # for hand_world_landmarks in results.multi_hand_world_landmarks:
-        #     mp_drawing.plot_landmarks(
-        #         hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5
-        #     )
 main("images/hand.jpeg")
